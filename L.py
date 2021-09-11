8d8918b2bbaf5259bb57254b8eb8bb4427d227a1
Timyr486786866745 <miron192837465@gmail.com>
2021-09-11 18:54:11 +0300

    Add L.py

--- L.py
+++ L.py
@@ -4,18 +4,18 @@ sys.stdout = open('output.txt', 'w')
  
 a = [] 
  
-def f1(x): 
-    f2 = [] 
+def func(x): 
+    final = [] 
     for i in x: 
         for j in str(i): 
             if j != '-': 
-                f2.append(int(j)) 
-    return sum(f2) 
+                final.append(int(j)) 
+    return sum(final) 
  
 for i in range(int(input().split()[0])): 
     a.append([int(i) for i in input().split()]) 
  
-a.sort(key=f1) 
+a.sort(key=func) 
  
 for i in range(len(a)): 
     for j in a[i]: 
