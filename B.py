2d0d6dfc1f4eb4da8dd93a2a923001c80b0a6193
Timyr486786866745 <miron192837465@gmail.com>
2021-09-11 18:22:30 +0300

    Add B

--- B.py
+++ B.py
@@ -1,11 +1,32 @@
-
-import sys
-sys.stdin = open('input.txt', 'r')
-sys.stdout = open('output.txt', 'w')
-
-for i in renge(5):
-    a, b, c =[int(i) for i in input().split()]
-    if a <=23 and b <= 59 and c <= 59:
-        print("YES")
-    else:
-        print("NO")
+import sys 
+sys.stdin = open('input.txt', 'r') 
+sys.stdout = open('output.txt', 'w') 
+(M,D) = [int(s) for s in input().split()] 
+if M<=0 and D<=0: 
+    print(0) 
+elif D==2 and M==2: 
+    print(2) 
+elif M%2==0 and M%2==0: 
+    if (M*2)==(D*2): 
+        print(1) 
+    elif M>D: 
+        print(1) 
+    elif M<D: 
+        print(2) 
+elif D%2!=0 and M%2!=0: 
+    if M>D: 
+        print(1) 
+    elif M<D: 
+        print(2) 
+elif M%2==0 and D%2!=0: 
+    if M>D: 
+        print(1) 
+    elif M<D: 
+        print(2) 
+elif M%2!=0 and D%2==0: 
+    if M>D: 
+        print(1) 
+    elif M<D: 
+        print(2) 
+elif M==D: 
+    print(0)
